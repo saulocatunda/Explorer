d1a8901759c0cc18431094a07881cf6f9eab29a4
Saulo Catunda <48536369+saulocatunda@users.noreply.github.com>
2022-09-27 10:23:59 -0300

    conceitosJS

---
+++ stage4/scripts.js
@@ -0,0 +1,68 @@
+/*Exercício 1
+let weight
+console.log(weight);
+*/
+
+/* Exercício 2
+let name = "Saulo"
+let age = 30
+let stars = 4.9
+let isSubscribed = true
+
+
+console.log(name, age, stars, isSubscribed);
+*/
+
+//Exercício 3
+
+let student = {
+  name: 'Saulo',
+  age: 30,
+  stars: 4.9,
+  isSubscribed: true,
+  weight: 90
+}
+
+/* Exercício 4
+console.log(
+  `O ${student.name} com ${student.age} de idade, pesa ${student.weight} quilos`
+)
+*/
+// Exercício 5
+let students = []
+
+//Exercício 6
+
+
+
+const john = {
+  name: 'John',
+  age: 23,
+  weight: 80,
+  isSubscribed: true
+}
+
+students = [
+  student,
+  john
+]
+
+// ou students[1] = john
+//Exercício 7
+/*
+console.log(students[0]);
+*/
+
+//Exercício 8
+
+
+
+console.log(students[1]);
+
+
+
+console.log(a)
+var a = 1;
+//Acertei, undefined, não existe ainda no momento do console.
+let b = 2;
+console.log(b);
