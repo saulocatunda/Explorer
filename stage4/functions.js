095b8f8bbca00b1ec3aa65ef27176d5681d4e48c
Saulo Catunda <48536369+saulocatunda@users.noreply.github.com>
2022-10-03 21:12:40 -0300

    exercise 0.1

---
+++ stage4/functions.js
@@ -0,0 +1,14 @@
+// function expression - function anonymous
+
+//Parâmetros (Parameters)
+const sum = function (number1, number2) {
+  total = number1 + number2
+  return total
+}
+// arguments
+// sum(2,3)
+
+console.log(`A soma total dos valores é `)
+
+let string = '123'
+console.log(typeof Number(string))
---
+++ stage4/functions2.js
@@ -0,0 +1,14 @@
+// Aplicativo de frases motivacionais.
+
+// Declaration - Declaração da função. Function Statement
+function createPhrases() {
+  console.log('Estudar pode ser muito bom');
+  console.log('Paciência e persistência');
+  console.log('Revisão é a mãe do aprendizado');
+}
+
+
+// Executar a função. execute, run, call, invoke
+createPhrases()
+
+console.log('fim do programa');
