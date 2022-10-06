c8e5b11f9c197fe3cda42f95320b750f52abcb9f
Saulo Catunda <48536369+saulocatunda@users.noreply.github.com>
2022-10-06 18:59:32 -0300

    stage4pt2

---
+++ stage4/introductionStage4/functions.js
@@ -0,0 +1,39 @@
+// function expression - function anonymous
+/*
+//Parâmetros (Parameters)
+const sum = function (number1, number2) {
+  total = number1 + number2
+  return total
+}
+// arguments
+// sum(2,3)
+
+console.log(`A soma total dos valores é `)
+
+let string = '123'
+console.log(typeof Number(string))
+
+let word = "Paralelepipido"
+
+console.log(word.length);
+
+let number = 1234
+
+console.log(String(number).length);
+
+let number = 326.5454454
+
+console.log(number.toFixed(2).replace(".",","));
+*/
+
+// let temperature = 35.6
+
+// if (temperature > 37.5) {
+//   console.log('Está com febre alta.')
+// } else if (temperature <= 37.5 && temperature >= 37) {
+//   console.log('A febre está moderada.')
+// } else {
+//   console.log('Não está com febre')
+// }
+
+
