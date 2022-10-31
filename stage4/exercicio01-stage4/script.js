520660117c0dd17cf67d06c873255fe77852c211
Saulo Catunda <48536369+saulocatunda@users.noreply.github.com>
2022-10-31 17:30:52 -0300

    exercise01-stage4-finished

--- stage4/exercicio01-stage4/script.js
+++ stage4/exercicio01-stage4/script.js
@@ -3,17 +3,22 @@ let numbers = [0, 1]
 for (const number of numbers) {
   numbers[number] = prompt('Digite dois números: ')
 }
-
-function numbersCalculated (num1, num2) {
-  
-}
+let numbersCalculated = [4]
 
 const operators = ['+', '-', '*', '/', '%']
 
-for (const operator of operators) {
+function calculator(num1, num2) {
+  numbersCalculated[0] = num1 + num2
+  numbersCalculated[1] = num1 - num2
+  numbersCalculated[2] = num1 * num2
+  numbersCalculated[3] = (num1 / num2).toFixed(2)
+  numbersCalculated[4] = num1 % num2
+}
+
+calculator(Number(numbers[0]), Number(numbers[1]))
+
+for (let index = 0; index < numbersCalculated.length; index++) {
   alert(
-    `Cálculos dos números fornecido: ${numbers[0]} ${operator} ${
-      numbers[1]
-    } = ${} `
+    `Cálculos dos números fornecido: ${numbers[0]} ${operators[index]} ${numbers[1]} = ${numbersCalculated[index]}`
   )
 }
