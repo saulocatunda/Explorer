24d929921d67901a04e737abfdb0cb514f691f2d
Saulo Catunda <48536369+saulocatunda@users.noreply.github.com>
2022-12-14 16:56:04 -0300

    problem-with-animation

--- stage5/biscoito-da-sorte/main.js
+++ stage5/biscoito-da-sorte/main.js
@@ -1,3 +1,17 @@
-const screen1 = document.querySelector('.screen1');
-const screen2 = document.querySelector('.screen2');
-const imgScreen1 = document.querySelector('.screen1 img')
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const imgScreen1 = document.querySelector('#closed-cookie')
+const btnScreen2 = document.querySelector('#buttonReset')
+
+imgScreen1.addEventListener('click', cookieClick)
+btnScreen2.addEventListener('click', toggleScreen)
+
+function cookieClick() {
+  toggleScreen()
+
+}
+
+function toggleScreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
