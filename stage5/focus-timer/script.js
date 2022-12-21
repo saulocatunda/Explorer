fb278df43c08d342fdef70d3373cf38b42032075
Saulo Catunda <48536369+saulocatunda@users.noreply.github.com>
2022-12-21 10:51:23 -0300

    fixing-javascript-error

--- stage5/focus-timer/script.js
+++ stage5/focus-timer/script.js
@@ -4,35 +4,67 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+let minutes
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 console.log(minutesDisplay.textContent)
 
-buttonPlay.addEventListener('click', pauseEvent)
-buttonPause.addEventListener('click', pauseEvent)
-buttonStop.addEventListener('click', resetButton)
-buttonSoundOn.addEventListener('click', muteSound)
-buttonSoundOff.addEventListener('click', muteSound)
-buttonSet.addEventListener('click', () => {
-  let minutes = prompt('Quantos minutos?')
-  minutesDisplay.textContent = minutes
-})
+function countdown() {
+  setTimeout(() => {
+    let minutesCountdown = Number(minutesDisplay.textContent)
+    let secondsCountdown = Number(secondsDisplay.textContent)
 
-function pauseEvent() {
+    secondsDisplay.textContent = String(secondsCountdown - 1).padStart(2, '0')
+
+    if (minutesCountdown <= 0) {
+      buttonStop.classList.add('hide')
+      buttonPlay.classList.remove('hide')
+      buttonPause.classList.add('hide')
+      buttonSet.classList.remove('hide')
+      return
+    }
+
+    if (secondsCountdown <= 0) {
+      secondsCountdown = 60
+      minutesDisplay.textContent = String(minutesCountdown - 1).padStart(2, '0')
+    }
+
+    secondsDisplay.textContent = String(secondsCountdown - 1).padStart(2, '0')
+    countdown()
+  }, 1000)
+}
+
+buttonPlay.addEventListener('click', () => {
   buttonPlay.classList.toggle('hide')
   buttonPause.classList.toggle('hide')
-  buttonStop.classList.toggle('hide')
-  buttonSet.classList.toggle('hide')
-}
+  buttonStop.classList.remove('hide')
+  buttonSet.classList.add('hide')
+  countdown()
+})
 
-function resetButton() {
+buttonPause.addEventListener('click', () => {
+  buttonPlay.classList.toggle('hide')
+  buttonPause.classList.toggle('hide')
+})
+
+buttonStop.addEventListener('click', () => {
+  buttonStop.classList.add('hide')
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
-  buttonStop.classList.add('hide')
   buttonSet.classList.remove('hide')
-}
+})
 
-function muteSound() {
-  buttonSoundOn.classList.toggle('hide')
-  buttonSoundOff.classList.toggle('hide')
-}
+buttonSoundOn.addEventListener('click', () => {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
+})
+
+buttonSoundOff.addEventListener('click', () => {
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
+})
+
+buttonSet.addEventListener('click', () => {
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = String(minutes).padStart(2, '0')
+})
