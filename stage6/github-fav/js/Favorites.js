60741e09b963626b878c8406fe23653c2780729f
Saulo Catunda <48536369+saulocatunda@users.noreply.github.com>
2023-02-23 21:22:09 -0300

    Finished- GitFav

--- stage6/github-fav/js/Favorites.js
+++ stage6/github-fav/js/Favorites.js
@@ -1,35 +1,38 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`
-
-    return fetch(endpoint)
-      .then(data => data.json())
-      .then(({ login, name, public_repos, fallowers }) => ({
-        login,
-        name,
-        public_repos,
-        fallowers
-      }))
-  }
-}
-
+import { GithubUser } from './GithubUser.js'
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
-    this.tbody = this.root.querySelector('table tbody')
     this.load()
-    GithubUser.search('maykbrito').then(user => console.log(user))
   }
 
   load() {
-    const entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
-    console.log(entries)
-    this.entries = []
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
   }
 
   async add(username) {
-    const user = await GithubUser.search(username)
-    console.log(user)
+    try {
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if (userExists) {
+        throw new Error('Usuário já cadastrado')
+      }
+
+      const user = await GithubUser.search(username)
+
+      if (user.login === undefined) {
+        throw new Error('Usuário não encontrado!')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+    } catch (error) {
+      alert(error.message)
+    }
   }
 
   delete(user) {
@@ -38,6 +41,7 @@ export class Favorites {
     )
     this.entries = filteredEntries
     this.update()
+    this.save()
   }
 }
 
@@ -45,6 +49,7 @@ export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
 
+    this.tbody = this.root.querySelector('table tbody')
     this.update()
     this.onadd()
   }
@@ -55,7 +60,6 @@ export class FavoritesView extends Favorites {
       const { value } = this.root.querySelector('.search input')
 
       this.add(value)
-      console.log(value)
     }
   }
 
@@ -64,14 +68,16 @@ export class FavoritesView extends Favorites {
 
     this.entries.forEach(user => {
       const row = this.createRow()
+
       row.querySelector(
         '.user img'
       ).src = `https://github.com/${user.login}.png`
       row.querySelector('.user img').alt = `Imagem de ${user.name}`
       row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
-      row.querySelector('.fallowers').textContent = user.fallowers
+      row.querySelector('.followers').textContent = user.followers
 
       row.querySelector('.remove').onclick = () => {
         const isOk = confirm('Tem certeza que deseja deletar essa linha?')
@@ -98,9 +104,11 @@ export class FavoritesView extends Favorites {
                 <span>saulocatunda</span>
               </a>
             </td>
-            <td class="repositories">20</td>
-            <td class="fallowers">1</td>
-            <td><button class="remove">&times;</button></td>
+            <td class="repositories">0</td>
+            <td class="followers">0</td>
+            <td>
+              <button class="remove">&times;</button>
+            </td>
     `
 
     return tr
---
+++ stage6/github-fav/notes.md
@@ -0,0 +1,12 @@
+# Desafíos
+
+- Uso de Modulo no JS
+- Estilo Tabelas no CSS
+- pseudo elements
+- OO JavaScript [Herança, Polomorfismo]
+- Try, Catch, Throw
+- Higher Order Function
+- Imutabilidade de dados - Princípios Prog. Funcional
+- DOM [Preencher e criar elementos, criar]
+- JSON [API, Manipulação]
+
