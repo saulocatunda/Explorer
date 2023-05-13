79fab45fc060cddb0e2d4b29e8bce8bc664bac09
saulocatunda <saulogscatunda@gmail.com>
2023-05-13 18:43:29 -0300

    first commit on startingJS project

---
+++ stage4/startingJS/main.js
@@ -0,0 +1,20 @@
+const messageDisplay = document.querySelector("#message");
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3"); 
+const button4 = document.querySelector("#button4");
+const button5 = document.querySelector("#button5");
+const button6 = document.querySelector("#button6");
+const button7 = document.querySelector("#button7");
+const button8 = document.querySelector("#button8");
+const button9 = document.querySelector("#button9");
+const button10 = document.querySelector("#button10");
+
+button1.addEventListener("click", () => {
+messageDisplay.innerHTML = "Hello World!"
+});
+
+button2.addEventListener("click", () => {
+  
+});
